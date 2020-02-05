de0264e571a57d09b1119aae106a6d9b4d2fccae
gilbertohnrq <gilbertohnrq@gmail.com>
2020-02-04 23:49:01 -0300

    Creating the base for the app

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-sm32b.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+//Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+
+//Query params: request.query (Filtros, ordenação, paginação...)
+//Route params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para a criação ou alteração de um registro)
